198a727195abd223849500f91f0946e848ffa058
Carlos Cuevas <ccuevas@mdlive.com>
2025-05-06 14:18:33 -0700

    added fibonacci function

--- MagicalCreatureCatalog.playground/Contents.swift
+++ MagicalCreatureCatalog.playground/Contents.swift
@@ -1,65 +1,114 @@
+func fibonacciAbility(_ numbrToStopAt: Int) -> Int{
+      if numbrToStopAt < 0 {
+        return 0
+      }
+
+      var fibonacciSeries: [Int] = []
+
+      for index in 0...numbrToStopAt {
+        if index < 2 {
+          fibonacciSeries.append(index)
+        } else {
+          let element1 = fibonacciSeries[fibonacciSeries.count-1]
+          let element2 = fibonacciSeries[fibonacciSeries.count-2]
+          fibonacciSeries.append(element1 + element2)
+        }
+      }
+
+    //  print(fibonacciSeries)
+
+      return fibonacciSeries[numbrToStopAt]
+}
+
 //Struct definition
 
 struct Dragon {
-    let name: String
-    let description: String
-    let isGood: Bool
-    let magicPower: Int
+    private let name: String
+    private let description: String
+    private let isGood: Bool
+    private let magicPower: Int
+    private let abilityPower: Int
     
     init(name:String, descripton:String, isGood:Bool, magicalPower:Int) {
         self.name = name
         self.description = descripton
         self.isGood = isGood
         self.magicPower = magicalPower
+        self.abilityPower = fibonacciAbility(magicalPower)
+    }
+    
+    func ability(){
+        print("Breath Fire that causes \(abilityPower) pts of damage")
     }
 }
 
 struct Wizard {
-    let name: String
-    let description: String
-    let isGood: Bool
-    let magicPower: Int
+    private let name: String
+    private let description: String
+    private let isGood: Bool
+    private let magicPower: Int
+    private let abilityPower: Int
     
     init(name:String, descripton:String, isGood:Bool, magicalPower:Int) {
         self.name = name
         self.description = descripton
         self.isGood = isGood
         self.magicPower = magicalPower
+        self.abilityPower = fibonacciAbility(magicalPower)
+    }
+    func ability(){
+        print("Cast an spell that causes \(abilityPower) pts of damage")
     }
 }
 
 struct Elf {
-    let name: String
-    let description: String
-    let isGood: Bool
-    let magicPower: Int
+    private let name: String
+    private let description: String
+    private let isGood: Bool
+    private let magicPower: Int
+    private let abilityPower: Int
     
     init(name:String, descripton:String, isGood:Bool, magicalPower:Int) {
         self.name = name
         self.description = descripton
         self.isGood = isGood
         self.magicPower = magicalPower
+        self.abilityPower = fibonacciAbility(magicalPower)
+    }
+    func ability(){
+        print("Throws a magic dust that causes \(abilityPower) pts of damage")
     }
 }
 
 
 struct Fairy {
-    let name: String
-    let description: String
-    let isGood: Bool
-    let magicPower: Int
+    private let name: String
+    private let description: String
+    private let isGood: Bool
+    private let magicPower: Int
+    private let abilityPower: Int
     
     init(name:String, descripton:String, isGood:Bool, magicalPower:Int) {
         self.name = name
         self.description = descripton
         self.isGood = isGood
         self.magicPower = magicalPower
+        self.abilityPower = fibonacciAbility(magicalPower)
+    }
+    
+    func ability(){
+        print("Throws a magic dust that causes \(abilityPower) pts of damage")
     }
 }
 
-let gandalfTheGrayWizard = Wizard(name: "Gandalf", descripton: "a figure of wisdom, determination but humble and friendly", isGood: true, magicalPower: 30)
-let gandalfTheWhiteWizard = Wizard(name: "Gandalf", descripton: "a figure of greate authority and power, imbued with a clearer purpose", isGood: true, magicalPower: 100)
-let smaugTheDragon = Dragon(name: "Smaug", descripton: "the magifisent and terrifying fire-breathing creature of immense size and greed", isGood: false, magicalPower: 100)
-let naviTheFary = Fairy(name: "Navi", descripton: "she is known for her constant and sometime annoying cries of 'Hey!'", isGood: true, magicalPower: 10)
-let elf = Elf(name: "Dobby", descripton: "he used to be an salve, but he is no more, he is a free elf", isGood: true, magicalPower: 50)
+let gandalfTheGrayWizard = Wizard(name: "Gandalf", descripton: "a figure of wisdom, determination but humble and friendly", isGood: true, magicalPower: 3)
+let gandalfTheWhiteWizard = Wizard(name: "Gandalf", descripton: "a figure of greate authority and power, imbued with a clearer purpose", isGood: true, magicalPower: 10)
+let smaugTheDragon = Dragon(name: "Smaug", descripton: "the magifisent and terrifying fire-breathing creature of immense size and greed", isGood: false, magicalPower: 10)
+let naviTheFary = Fairy(name: "Navi", descripton: "she is known for her constant and sometime annoying cries of 'Hey!'", isGood: true, magicalPower: 1)
+let elf = Elf(name: "Dobby", descripton: "he used to be an salve, but he is no more, he is a free elf", isGood: true, magicalPower: 5)
 
+gandalfTheGrayWizard.ability()
+gandalfTheWhiteWizard.ability()
+smaugTheDragon.ability()
+naviTheFary.ability()
+elf.ability()
